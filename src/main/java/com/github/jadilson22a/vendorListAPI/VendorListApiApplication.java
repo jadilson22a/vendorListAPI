5c8a9dfb661f0074fd7ce3d25aa957eff956ab18
jadilson22a <jadilsonalves777.ma@outlook.com>
2025-11-20 18:03:11 -0300

    Estruturando Projeto SpringBoot

---
+++ src/main/java/com/github/jadilson22a/vendorListAPI/VendorListApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jadilson22a.vendorListAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jadilson22a/vendorListAPI/VendorListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jadilson22a.vendorListAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
